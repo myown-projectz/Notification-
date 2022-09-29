10803a2d532fc0d498496d1ab84ba933eea0c67a
sravan reddy <sravanr942@gmail.com>
2022-09-30 00:29:09 +0530

    added some thing

--- user.java
+++ user.java
@@ -2,11 +2,19 @@
  hai frds how are you man
  ghdcvhbv
  d cds cbsd c
+=======
+>>>>>>> dev
 
 
 
 
+<<<<<<< HEAD
 
 
  sravan is a good boy..
  i love you my country
+
+
+ lavanya is good girl s
+ bsbcv  scbshv
+>>>>>>> dev
